614c1f29c344566cbc4f7f566b21c9f8b8597c0b
Max <41009925+MAKblC@users.noreply.github.com>
2024-07-12 12:06:30 +0300

    Create settings.h

---
+++ project/settings.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID "XXXXX" // логин Wi-Fi
+#define WIFI_PASSWORD "XXXXXXXXXX" // пароль Wi-Fi
+
+#define BOT_TOKEN "91XXXXXXXXX90:AAXXXXXXXXXXXXXXXXXXXXXXXX4PUc" // токен бота
+#define your_chat_ID "28XXXXXXXXXX7"  // ID вашего чата с ботом
+
+String GOOGLE_SCRIPT_ID = "AKfXXXXXXXXXXXXXXXXXYD6tOGhDXXXXXXXXXXXXXXXXXXGOWPoWSSdA";  // Script ID для гугл таблицы
